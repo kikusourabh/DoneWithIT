e4342fb2dea6d71e77e625dc20d8b75551f3e9ac
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-12 15:31:30 +0530

    touchables done

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Constants from "expo-constants";
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableNativeFeedback,
+  TouchableHighlight,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   // function to call on text onPress event
@@ -12,20 +21,31 @@ export default function App() {
       <Text numberOfLines={1} onPress={handlePress}>
         Hello react native
       </Text>
+      {/* touchables */}
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        {/* for loading images from url */}
+        <Image
+          blurRadius={10}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      <TouchableOpacity>
+        {/* for loading local images from the assets folder */}
+        <Image source={require("./assets/favicon.png")} />
+      </TouchableOpacity>
 
-      {/* for loading local images from the assets folder */}
-      <Image source={require("./assets/icon.png")} />
+      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
+        {/* for loading images from url */}
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "#8fc0a9" }}
+        ></View>
+      </TouchableNativeFeedback>
 
-      {/* for loading images from url */}
-      <Image
-        blurRadius={10}
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
