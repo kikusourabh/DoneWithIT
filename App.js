99c577066f56fbeea3955a5f26c7ba13ee8c5984
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-12 15:59:33 +0530

    stylesheet code

--- App.js
+++ App.js
@@ -1,47 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Constants from "expo-constants";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  TouchableNativeFeedback,
-  TouchableHighlight,
-  Image,
-  TouchableOpacity,
-  Button,
-  Alert,
-} from "react-native";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
 
 export default function App() {
   // function to call on text onPress event
   const handlePress = () => console.log("text pressed");
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, blueBackground]}>
       <Button
         color="#8fc0a9"
-        title="Alert button"
-        onPress={() =>
-          Alert.alert("my title", "button tap", [
-            { text: "yes", onPress: () => console.log("pressed yes") },
-            { text: "no", onPress: () => console.log("pressed no") },
-          ])
-        }
+        title="Click me"
+        onPress={() => console.log("pressed")}
       />
-      <Button
-        color="#8fc0a9"
-        title="promet button"
-        onPress={() =>
-          Alert.prompt("my title", "button tap", (text) => console.log(text))
-        }
-      />
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
-
+const blueBackground = {
+  backgroundColor: "#ffc1f3",
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
