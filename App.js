19cf52e6de5d49bc68712b609ec3d3ad1a4fcfb2
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-12 16:08:15 +0530

    platform keywords

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Constants from "expo-constants";
-import { StyleSheet, SafeAreaView, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Button, Platform } from "react-native";
 
 export default function App() {
   // function to call on text onPress event
   const handlePress = () => console.log("text pressed");
 
   return (
-    <SafeAreaView style={[styles.container, blueBackground]}>
+    <SafeAreaView style={[styles.container]}>
       <Button
         color="#8fc0a9"
         title="Click me"
@@ -19,15 +19,10 @@ export default function App() {
     </SafeAreaView>
   );
 }
-const blueBackground = {
-  backgroundColor: "#ffc1f3",
-};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffffff",
-    justifyContent: "center",
-    alignItems: "center",
     paddingTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
   },
 });
