05430974ea3eb0c106c34733f349e04d1cdc6307
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-13 14:05:04 +0530

    name state changes

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import Constants from "expo-constants";
-import { View } from "react-native";
+import { View, StyleSheet, Text, Button } from "react-native";
 import WlecomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  //state
+  const [name, setName] = useState("sourabh");
+
+  const pressHandler = () => {
+    //checking the sate and proceed on base of the condition
+    if (name === "sourabh") {
+      setName("sourabh karmakar");
+    } else {
+      setName("sourabh");
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Text>My name is {name}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update name" onPress={pressHandler} />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+});
