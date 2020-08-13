821d3299bfe8a29a26451e9efc486ba7b1b9605f
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-13 11:47:30 +0530

    createded  a screen

--- App.js
+++ App.js
@@ -2,42 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Constants from "expo-constants";
 import { View } from "react-native";
+import WlecomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#ffffff",
-        flex: 1,
-        flexDirection: "row", //horizontal
-        justifyContent: "center", //primary
-        alignItems: "center", //secondary
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "#ffa36c",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "#ebdc87",
-          width: 100,
-          top: 20,
-          left: 20,
-          position: "absolute", // relative as default
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "#799351",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
