8c2ca2f45a7fdab6f75035a773e8a90d472a3430
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-13 16:23:43 +0530

    flat list

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
   TextInput,
   SafeAreaView,
   ScrollView,
+  FlatList,
 } from "react-native";
 import WlecomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
@@ -17,29 +18,37 @@ import color from "./app/config/color";
 export default function App() {
   //state
   const [people, setPeople] = useState([
-    { name: "sourabh", key: "1" },
-    { name: "rahul", key: "2" },
-    { name: "velsi", key: "3" },
-    { name: "akash", key: "4" },
-    { name: "bhavesh", key: "5" },
-    { name: "simit", key: "6" },
-    { name: "ritika", key: "7" },
-    { name: "soumen", key: "8" },
-    { name: "nikhil", key: "9" },
-    { name: "sreya", key: "10" },
-    { name: "arpita", key: "11" },
-    { name: "rohan", key: "12" },
+    { name: "sourabh", id: "1" },
+    { name: "rahul", id: "2" },
+    { name: "velsi", id: "3" },
+    { name: "akash", id: "4" },
+    { name: "bhavesh", id: "5" },
+    { name: "simit", id: "6" },
+    { name: "ritika", id: "7" },
+    { name: "soumen", id: "8" },
+    { name: "nikhil", id: "9" },
+    { name: "sreya", id: "10" },
+    { name: "arpita", id: "11" },
+    { name: "rohan", id: "12" },
   ]);
-
+  const PersonItem = ({ item }) => (
+    <Text style={styles.personContainer}>{item.name}</Text>
+  );
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
+      <FlatList
+        numColumns={3}
+        data={people}
+        renderItem={PersonItem}
+        keyExtractor={(item) => item.id}
+      />
+      {/* <ScrollView>
         {people.map((person) => (
           <View key={person.key}>
             <Text style={styles.person}>{person.name}</Text>
           </View>
         ))}
-      </ScrollView>
+      </ScrollView> */}
     </SafeAreaView>
   );
 }
@@ -49,12 +58,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: color.white,
   },
-  person: {
+  personContainer: {
     marginTop: 24,
     marginLeft: 20,
     marginRight: 20,
     padding: 20,
     backgroundColor: color.secondary,
     fontSize: 16,
+    justifyContent: "center",
   },
 });
