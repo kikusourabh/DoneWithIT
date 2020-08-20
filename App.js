992f4ce1a30ca5eb021ebe0382a09f78af683af0
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-20 13:23:27 +0530

    basics

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React, { useState, useRef } from "react";
-import { View, Button, Animated } from "react-native";
+import { View, Button, Animated, Text } from "react-native";
 import color from "./app/config/color";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function App() {
+  const animeValue = useState(new Animated.Value(0))[0];
+  const move = () => {
+    Animated.spring(animeValue, {
+      toValue: 300,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+  };
+
   return (
     <View
       style={{
         flex: 1,
       }}
-    ></View>
+    >
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Animated.View
+          style={{
+            height: 100,
+            width: 100,
+            marginLeft: animeValue,
+            borderRadius: 100 / 2,
+            backgroundColor: color.WindowBackground,
+          }}
+        />
+        <TouchableOpacity onPress={move}>
+          <Text>Move me</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
